c46e34000008f51c0607b7479ccd1bf3bc6370d1
gingivitiss <39242221+gingivitiss@users.noreply.github.com>
2019-12-29 15:26:03 +0800

    Add mouse click player movement

--- unnamed-game/Player.gd
+++ unnamed-game/Player.gd
@@ -1,40 +1,16 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export (int) var speed = 200
+
+var target = Vector2()
 var velocity = Vector2()
-const UP = Vector2(0,-1)
 
-const ACCELERATION = 200
-const MAXSPEED = 600
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _input(event):
+	if event.is_action_pressed('skate_to'):
+		target = get_global_mouse_position()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	_movement()
-	
-func _movement():
-	if Input.is_action_just_pressed("ui_left"):
-		
-		velocity.x = max(velocity.x-ACCELERATION,-MAXSPEED)
-
-	elif Input.is_action_just_pressed("ui_right"):
-		velocity.x = min(velocity.x+ACCELERATION,MAXSPEED)
-		print(velocity.x)
-	
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = min(velocity.y+ACCELERATION,MAXSPEED)
-	elif Input.is_action_pressed("ui_up"):
-		velocity.y = max(velocity.y-ACCELERATION,-MAXSPEED)
-#		
-	else:
-		velocity.x = lerp(velocity.x,0,0.01)
-		velocity.y = lerp(velocity.y,0,0.03)
-	if velocity.length() > 0:
-		velocity.normalized() * MAXSPEED
-	velocity = move_and_slide(velocity)
-	pass
+	velocity = (target - position).normalized() * speed
+	# rotation = velocity.angle()
+	if (target - position).length() > 5:
+		velocity = move_and_slide(velocity)
